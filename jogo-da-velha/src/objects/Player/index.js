94059b8f27c2acfaca23bf2c9d3072fcb4ecae9c
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-03-28 10:18:37 -0300

    aceitar 2 jogadores no player

--- jogo-da-velha/src/objects/Player/index.js
+++ jogo-da-velha/src/objects/Player/index.js
@@ -1,8 +1,22 @@
 import React from "react";
+import playerX from '../../img/player-x.png';
+import playerO from '../../img/player-o.png';
+import "./styles.css";
 
-const Player = () => {
+const Player = (props) => {
+    let player = "";
+    if (props.player === "x") {
+        player = playerX;
+    } else if (props.player === "o") {
+        player =  playerO;
+    } else {
+        alert("Jogador não identificado")
+    }
+    
     return (
-        <button className="player">X</button>
+        <button className="player">
+            <img src={player} alt={`Jogador ${props.player.toUpperCase()}`}/>
+        </button>
     )
 }
 
