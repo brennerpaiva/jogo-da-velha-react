c4f7ef4c955a95eadf912c37e8c3eeb66dffbd7a
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-03-28 14:53:43 -0300

    Component Hashtag criado e Layour basico definido

---
+++ jogo-da-velha/src/components/Hashtag/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from '../../objects/Card';
+import Player from '../../objects/Player';
+import './styles.css'
+
+const Hashtag = () => {
+    return (
+        <Card>
+            <ul className='hashtag'>
+                <li className='item'><Player player="x"/></li>
+                <li className='item'><Player player="o"/></li>
+                <li className='item'><Player player="x"/></li>
+                
+                <li className='item'><Player player="o"/></li>
+                <li className='item'><Player player="x"/></li>
+                <li className='item'><Player player="o"/></li>
+                
+                <li className='item'><Player player="x"/></li>
+                <li className='item'><Player player="o"/></li>
+                <li className='item'><Player player="x"/></li>
+            </ul>
+      </Card>
+    )
+}
+
+export default Hashtag
